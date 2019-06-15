434cc229534bf061a11bd5e45f10b4d293953c0a
Oat <idiot_genesis@hotmail.com>
2019-06-15 15:16:58 +0700

    px-pawn

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,18 +1,17 @@
 export const environment = {
   key: '42444678902180239428358646519815165132078951561321646432131696998878616',
   production: true,
-  appName: '',
-  appNameEng: '',
-  appAcronym: '', //login logo .png(200*200px), icon .svg, default is 'demo'
-  reportServer: 'http://172.28.1.10:8080/jasperserver/flow.html?',
-  reportSite: '',
+  appName: 'สำนักงานธนานุเคราะห์',
+  appNameEng: 'Office of The Government Pawshop',
+  appAcronym: 'pawn', //login logo .png(200*200px), icon .svg, default is 'demo'
+  reportServer: 'http://127.0.0.1:8080/jasperserver/flow.html?',
+  reportSite: 'pawn',
   reportUser: 'jasperadmin',
   reportPass: 'jasperadmin',
-  //ng build --prod -bh /praxticol8_ghb/
+  //ng build --prod -bh /praxticol8_pawn/
   //apiServer: 'http://172.28.1.10:8080',
   apiServer: 'http://127.0.0.1:8080',
-  apiName: '/pxservice-ghb/api',
-  plugIn: 'http://172.28.1.10',
-  url: 'http://172.28.1.10/praxticol8_ghb/#'
+  apiName: '/pxservice-pawn/api',
+  plugIn: 'http://127.0.0.1',
+  url: 'http://127.0.0.1/praxticol8_pawn/#'
 };
-
