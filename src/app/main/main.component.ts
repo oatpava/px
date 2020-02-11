c98f339d626854df0a22c77ee0724d30dcd878ff
Sakul Pavapootanont <idiot_genesis2hotmail.com>
2020-02-11 23:59:42 +0700

    Auto stash before rebase of "refs/heads/master"

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -211,19 +211,19 @@ export class MainComponent implements OnInit {
     })
   }
 
-  // openDialogChangePassword(): void {
-  //   let dialogRef = this._dialog.open(ChangePasswordComponent, {
-  //     width: '40%',
-  //   });
-  //   dialogRef.afterClosed().subscribe(result => {
-  //     if (result) {
-  //       let dialogAlert = this._dialog.open(AlertMessageComponent, {
-  //         width: '40%',
-  //       });
-  //       dialogAlert.componentInstance.message = 'กรุณาเข้าระบบใหม่'
-  //     }
-  //   });
-  // }
+  openDialogChangePassword(): void {
+    let dialogRef = this._dialog.open(ChangePasswordComponent, {
+      width: '40%',
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        let dialogAlert = this._dialog.open(AlertMessageComponent, {
+          width: '40%',
+        });
+        dialogAlert.componentInstance.message = 'กรุณาเข้าระบบใหม่'
+      }
+    });
+  }
 
   goBack() {
     this._location.back()
