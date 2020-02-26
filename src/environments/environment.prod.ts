cd1e42177e1e5baf9161657d0aad1b782f210b5d
Sakul Pavapootanont <idiot_genesis2hotmail.com>
2020-02-27 02:28:46 +0700

    env.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,20 +1,20 @@
 export const environment = {
-  buildDate: '2020-02-22',
+  buildDate: '2020-02-27 02.05',
   key: '42444678902180239428358646519815165132078951561321646432131696998878616',
   production: true,
   appName: 'กรมอุตสาหกรรมพื้นฐานและการเหมืองแร่',
   appNameEng: 'Department of Primary Industries and Mines',
   appAcronym: 'dpim', //login logo .png(200*200px), icon .svg, default is 'demo'
-  reportServer: 'http://saraban62.dpim.go.th:85/jasperserver/flow.html?',
+  reportServer: 'http://saraban63.dpim.go.th:85/jasperserver/flow.html?',
   reportSite: '',
   reportUser: 'jasperadmin',
   reportPass: 'jasperadmin',
   //ng build --prod -bh /praxticol8_dpim/
   //apiServer: 'http://172.28.1.10:8080',
-  apiServer: 'http://saraban62.dpim.go.th:82',
+  apiServer: 'http://saraban63.dpim.go.th:82',
   apiName: '/pxservice-dpim/api',
-  plugIn: 'http://saraban62.dpim.go.th:80',
-  url: 'http://saraban62.dpim.go.th/praxticol8_dpim/#',
+  plugIn: 'http://saraban63.dpim.go.th:80',
+  url: 'http://saraban63.dpim.go.th/praxticol8_dpim/#',
   DMS: true
 };
 
