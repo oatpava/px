d3d24b5604a75568b35fef1b56c22554bda7501d
Sakul Pavapootanont <idiot_genesis@hotmail.com>
2020-08-22 17:34:41 +0700

    [ghb]
    e064301161569cb009166cec0f32fbc919fd6d9a

--- src/app/setting/history-log/history-log.component.ts
+++ src/app/setting/history-log/history-log.component.ts
@@ -1,91 +1,45 @@
-import { Component, OnInit, Input } from '@angular/core'
-import { Router, ActivatedRoute, Params } from '@angular/router'
+import { Component, OnInit, ViewChild } from '@angular/core'
 import { Location } from '@angular/common'
 import { TdLoadingService } from '@covalent/core'
-import { IMyOptions, IMyDateModel } from 'mydatepicker'
-import { MdDialog, MdDialogRef } from '@angular/material'
+import { IMyOptions } from 'mydatepicker'
+import { MdDialog, MdSidenav } from '@angular/material'
 import { URLSearchParams } from '@angular/http'
-
-import { TdDataTableService, TdDataTableSortingOrder, ITdDataTableSortChangeEvent, ITdDataTableColumn, ITdDataTableSelectEvent } from '@covalent/core';
-import { IPageChangeEvent } from '@covalent/core'
+import { DataTable } from 'primeng/primeng'
 
 import { ListHisttoryUserComponent } from '../../setting/history-log/list-histtory-user/list-histtory-user.component'
 import { ReportLogComponent } from '../../setting/history-log/report-log/report-log.component'
-
-
 import { PxService, } from '../../main/px.service'
-import { HistoryLog } from './model/history-log.model'
-import { HISTORYLOG } from './model/mock-history-log'
 import { HistoryLogService } from './history-log.service'
 import { ParamSarabanService } from '../../saraban/service/param-saraban.service'
+import { ListReturn } from '../../main/model/listReturn.model'
 
+const limit: number = 50
 @Component({
   selector: 'app-history-log',
   templateUrl: './history-log.component.html',
   styleUrls: ['./history-log.component.styl'],
-  providers: [HistoryLogService, PxService, ]
+  providers: [HistoryLogService, PxService,]
 })
 export class HistoryLogComponent implements OnInit {
+  @ViewChild('sideSubMenu') sidenav: MdSidenav
   listMenu: string = 'menu'
   hoverMenuEdit: boolean = true
   showMenu: boolean = true
   ModeSearch: boolean = true
-  data: any[] = [] //no source
   reportData: any
-  columns: ITdDataTableColumn[] = [
-    { name: 'moduleName', label: 'Modules' },
-    { name: 'userProfileName', label: 'ผู้ใช้งาน' },
-    { name: 'createdDate', label: 'วันที่' },
-    { name: 'ipAddress', label: 'IP' },
-    { name: 'description', label: 'รายละเอียด' },
-  ];
-  additional: any[] = [
-    { "checked": false,},
-    { "checked": false, type: 1, label: 'สร้าง' },
-    { "checked": false, type: 2, label: 'แก้ไข' },
-    { "checked": false, type: 3, label: 'ลบ' },
-    { "checked": false, type: 4, label: 'เข้าใช้งานระบบ' },
-    { "checked": false, type: 5, label: 'กู้คืน' },
-    { "checked": false, type: 6, label: 'พิมพ์' },
-    { "checked": false, type: 7, label: 'คัดลอก' },
-    { "checked": false, type: 8, label: 'นำออก' },
-    { "checked": false, type: 9, label: 'เปิดใช้' },
-    { "checked": false, type: 10, label: 'ออกจากระบบ' },
-    // { "checked": false,}
-  ];
-  type: any[]
-  filteredData: any[]
-  filteredTotal: number
-  searchTerm: string = '';
-  fromRow: number = 1;
-  currentPage: number = 1;
-  pageSize: number = 5;
-  sortBy: string = 'id';
-  sortOrder: TdDataTableSortingOrder = TdDataTableSortingOrder.Descending;
-  searchField: any
-  dataSearch: 'none'
+  logTypes: any = [
+    { name: 'logType1', "checked": false, type: 1, label: 'สร้าง' },
+    { name: 'logType2', "checked": false, type: 2, label: 'แก้ไข' },
+    { name: 'logType3', "checked": false, type: 3, label: 'ลบ' },
+    { name: 'logType4', "checked": false, type: 4, label: 'เข้าใช้งานระบบ' },
+    { name: 'logType5', "checked": false, type: 5, label: 'กู้คืน' },
+    { name: 'logType6', "checked": false, type: 10, label: 'ออกจากระบบ' },
+  ]
   modules: any[] = [
-    {
-      id: 1,
-      name: 'ทั้งหมด',
-      code: ''
-    }, {
-      id: 2,
-      name: 'หน้าจอส่วนตัว',
-      code: 'mwp'
-    }, {
-      id: 3,
-      name: 'ทะเบียนส่วนกลาง',
-      code: 'wf'
-    }, {
-      id: 4,
-      name: 'ระบบจัดเก็บเอกสารฯ',
-      code: 'dms'
-    }, {
-      id: 5,
-      name: 'ส่วนงานผู้ดูแลระบบ',
-      code: 'admin'
-    }
+    { id: 1, name: 'ทั้งหมด', code: '' },
+    { id: 2, name: 'หน้าจอส่วนตัว', code: 'mwp' },
+    { id: 3, name: 'ระบบสารบรรณฯ', code: 'wf' },
+    { id: 5, name: 'ผู้ดูแลระบบ', code: 'admin' }
   ]
   private myDatePickerOptions: IMyOptions = {
     dateFormat: 'dd/mm/yyyy',
@@ -97,13 +51,35 @@ export class HistoryLogComponent implements OnInit {
     openSelectorOnInputClick: true,
     showSelectorArrow: false,
   }
+  searchField: {
+    dateBegin: Date,
+    dateEnd: Date,
+    createdDateBegin: string,
+    createdDateEnd: string,
+    moduleName: string,
+    type: string,
+    description: string,
+    userName: string,
+    createdBy: string,
+  } = {
+      dateBegin: null,
+      dateEnd: null,
+      createdDateBegin: '',
+      createdDateEnd: '',
+      moduleName: '',
+      type: '',
+      description: '',
+      userName: '',
+      createdBy: ''
+    }
+
+  @ViewChild('dt') dt: DataTable
+  datas: any[] = []
+  listReturn = new ListReturn()
 
   constructor(
-    private _route: ActivatedRoute,
-    private _router: Router,
     private _loadingService: TdLoadingService,
     private _location: Location,
-    private _dataTableService: TdDataTableService,
     private _dialog: MdDialog,
     private _hisroyLogService: HistoryLogService,
     private _pxService: PxService,
@@ -112,100 +88,72 @@ export class HistoryLogComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.searchField = {
-      createdDateEnd: '',
-      createdDateBegin: '',
-      moduleName: '',
-      description: '',
-      userName: '',
-      createdBy: ''
+    this.getHistoryList(limit)
+  }
+
+  sideNavAlert(event) {
+    if (event.toElement.className === 'md-sidenav-backdrop') {
+      this.ModeSearch = true
     }
-
-    this.filter()
-    this.getHistoryList()
   }
 
-  sort(sortEvent: ITdDataTableSortChangeEvent): void {
-    this.sortBy = sortEvent.name;
-    this.sortOrder = sortEvent.order;
-    this.filter();
-  }
-  search(searchTerm: string): void {
-    this.searchTerm = searchTerm;
-    this.filter();
-  }
-  page(pagingEvent: IPageChangeEvent): void {
-    this.fromRow = pagingEvent.fromRow;
-    this.currentPage = pagingEvent.page;
-    this.pageSize = pagingEvent.pageSize;
-    this.filter();
-  }
-  filter(): void {
-    let newData: any[] = this.data;
-    newData = this._dataTableService.filterData(newData, this.searchTerm, true);
-    this.filteredTotal = newData.length;
-    newData = this._dataTableService.sortData(newData, this.sortBy, this.sortOrder);
-    newData = this._dataTableService.pageData(newData, this.fromRow, this.currentPage * this.pageSize);
-    this.filteredData = newData;
+  closeSideNave() {
+    this.sidenav.close()
+    this.ModeSearch = true
   }
 
-  getHistoryList() {  //no source
+  overMenu(value: string) {
+    this.hoverMenuEdit = false
+    this.listMenu = 'keyboard_arrow_up'
+  }
+
+  leaveMenu() {
+    this.hoverMenuEdit = true
+    this.listMenu = 'menu'
+  }
+
+  getHistoryList(limit: number) {
     this._loadingService.register('main')
     this._hisroyLogService
-      .getHistoryList()
+      .getSearchHistoryList(0, limit, this.searchField)
       .subscribe(response => {
-        console.log('getHistoryList', response)
         this._loadingService.resolve('main')
-        this.data = response.data
+        this.datas = response.data
+        this.listReturn = response.list
         this.reportData = response.listModule
       });
   }
 
-  sideNavAlert(e): void {
-    if (e.toElement.className === 'md-sidenav-backdrop') {
-      this.ModeSearch = true;
-    }
-  }
-
-  onDateChanged(event: any) {
-  }
-
-  searchLog(data,additional: any) {
-    data.type = ""
-    for(let i=0;i<this.additional.length;i++){
-      
-      if(this.additional[i].checked){
-        if(data.type == null || data.type == "" || data.type == "undefined"){
-          data.type = this.additional[i].type 
-        }else{
-          data.type += "฿"+this.additional[i].type 
-        }
+  prepareSearchModel(searchField: any): any {
+    for (let i = 0; i < this.logTypes.length; i++) {
+      if (this.logTypes[i].checked) {
+        searchField.type = this.logTypes[i].type
       }
     }
-
-    console.log('searchLog', data)
-    this._loadingService.register('main')
-    this._hisroyLogService
-      .getSearchHistoryList(this._hisroyLogService.convertDateFormat(data))
-      .subscribe(response => {
-        this._loadingService.resolve('main')
-        this.data = response.data
-        this.reportData = response.listModule
-
-        this.searchField = {
-          createdDateEnd: '',
-          createdDateBegin: '',
-          moduleName: '',
-          description: '',
-          userName: '',
-          createdBy: ''
-        }
-
-        this.additional.forEach(a => a.checked = false)
-      });
+    console.log('xxx', this.searchField)
+    searchField.createdDateBegin = this.getStringDate(searchField.dateBegin)
+    searchField.createdDateEnd = this.getStringDate(searchField.dateEnd)
+    return searchField
   }
 
-  getUser() {
+  getStringDate(date: any) {
+    return date ? ("0" + date.date.day).slice(-2) + "/" + ("0" + date.date.month).slice(-2) + "/" + (date.date.year) : ''
+  }
+
+  searchLog() {
+    this._loadingService.register('main')
+    this._hisroyLogService
+      .getSearchHistoryList(0, limit, this.prepareSearchModel(this.searchField))
+      .subscribe(response => {
+        this._loadingService.resolve('main')
+        this.datas = response.data
+        this.listReturn = response.list
+        this.reportData = response.listModule
+        this.closeSideNave()
+      })
+  }
+
+  openDialogUser() {
     let dialogRef = this._dialog.open(ListHisttoryUserComponent, {
       width: '50%',
     });
@@ -237,33 +185,46 @@ export class HistoryLogComponent implements OnInit {
     })
   }
 
-  overMenu(value: string) {
-    this.hoverMenuEdit = false
-    this.listMenu = 'keyboard_arrow_up'
-  }
-  leaveMenu() {
-    this.hoverMenuEdit = true
-    this.listMenu = 'menu'
-  }
-
   reportpdf() {
-    let data = this.data
+    let data = this.datas
     this._loadingService.register('main')
     this._hisroyLogService
       .tmpReport(data)
       .subscribe(response => {
         this._loadingService.resolve('main')
         let params = new URLSearchParams()
-
         params.set("jobType", 'log_Report')
         params.set("createdBy", '' + this._paramSarabanService.userId)
         this._pxService.report('log_Report', 'pdf', params)
-
       })
   }
 
-  private getDate(date: String): Date {
-    return date ? new Date(+date.substr(6, 4) - 543, +date.substr(3, 2) - 1, +date.substr(0, 2)) : null
+  resetDataTable() {
+    this.dt.reset()
+    this.searchField = {
+      dateBegin: null,
+      dateEnd: null,
+      createdDateEnd: '',
+      createdDateBegin: '',
+      moduleName: '',
+      type: '',
+      description: '',
+      userName: '',
+      createdBy: ''
+    }
+    this.getHistoryList(limit)
+  }
+
+  loadMoreContents() {
+    this._loadingService.register('main')
+    this._hisroyLogService
+      .getSearchHistoryList(this.listReturn.count, limit, this.prepareSearchModel(this.searchField))
+      .subscribe(response => {
+        this._loadingService.resolve('main')
+        this.datas = this.datas.concat(response.data)
+        this.listReturn = response.list
+        this.reportData = this.reportData.concat(response.listModule)
+      })
   }
 
 }
