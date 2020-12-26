ef4509897c9b2df9ac22392fc03a2bf4b23202fb
Sakul Pavapootanont <idiot_genesis@hotmail.com>
2020-12-26 17:25:23 +0700

    [c] logData limit to 1000

--- src/app/setting/history-log/history-log.component.ts
+++ src/app/setting/history-log/history-log.component.ts
@@ -13,7 +13,7 @@ import { HistoryLogService } from './history-log.service'
 import { ParamSarabanService } from '../../saraban/service/param-saraban.service'
 import { ListReturn } from '../../main/model/listReturn.model'
 
-const limit: number = 500
+const limit: number = 1000
 @Component({
   selector: 'app-history-log',
   templateUrl: './history-log.component.html',
