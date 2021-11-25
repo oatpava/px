b798b22ca2931cccddd71cb0e04a8da21dd9e2b6
Sakul Pavapootanont <idiot_genesis@hotmail.com>
2021-11-26 01:20:32 +0700

    [ENV]

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,19 +1,19 @@
 export const environment = {
-  buildDate: '2020-10-22 11:02',
+  buildDate: '2020-11-25 16:16',
   key: '42444678902180239428358646519815165132078951561321646432131696998878616',
   production: true,
-  appName: '',
-  appNameEng: '',
-  appAcronym: '', //login logo .png(200*200px), icon .svg, default is 'demo'
-  reportServer: 'http://127.0.0.1:85/jasperserver/flow.html?',
-  reportSite: '',
+  appName: 'กรมวิทยาศาสตร์การแพทย์',
+  appNameEng: 'Department of Medical Sciences Ministry of Public Health',
+  appAcronym: 'DMSC', //login logo .png(200*200px), icon .svg, default is 'demo'
+  reportServer: 'http://192.168.200.110:85/jasperserver/flow.html?',
+  reportSite: 'DMSC',
   reportUser: 'jasperadmin',
   reportPass: 'jasperadmin',
-  //ng build --prod -bh /praxticol8_px8/
-  apiServer: 'http://127.0.0.1:8080',
-  apiName: '/pxservice-px8/api',
-  plugIn: 'http://127.0.0.1:82',
-  url: 'http://127.0.0.1/praxticol8_px8/#',
+  //ng build --prod -bh /DMSC/
+  apiServer: 'http://192.168.200.110:8080',
+  apiName: '/dmscService/api',
+  plugIn: 'http://192.168.200.110:82',
+  url: 'http://192.168.200.110/DMSC#',
   DMS: false
 };
 
