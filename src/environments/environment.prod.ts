ce9f81332016d4beb48e5846c1ef597557ca5ddf
Sakul Pavapootanont <idiot_genesis@hotmail.com>
2022-01-16 17:42:40 +0700

    [c] appNameEng

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   key: '42444678902180239428358646519815165132078951561321646432131696998878616',
   production: true,
   appName: 'กรมวิทยาศาสตร์การแพทย์',
-  appNameEng: 'Department of Medical Sciences Ministry of Public Health',
+  appNameEng: 'Department of Medical Sciences',
   appAcronym: 'DMSC', //login logo .png(200*200px), icon .svg, default is 'demo'
   reportServer: 'http://192.168.200.110:85/jasperserver/flow.html?',
   reportSite: 'DMSC',
