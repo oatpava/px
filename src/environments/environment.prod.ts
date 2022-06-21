707afc25323cef7cc89a7162e0684d6c6a1b16d2
Sakul Pavapootanont <idiot_genesis@hotmail.com>
2022-06-22 06:34:25 +0700

    [ENV]

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  buildDate: '2020-10-22 11:02',
+  buildDate: '2022-06-20 06:20',
   key: '42444678902180239428358646519815165132078951561321646432131696998878616',
   production: true,
   appName: 'กรมอุตสาหกรรมพื้นฐานและการเหมืองแร่',
@@ -11,7 +11,7 @@ export const environment = {
   reportPass: 'jasperadmin',
   //ng build --prod -bh /praxticol8_dpim/
   //apiServer: 'http://172.28.1.10:8080',
-  apiServer: 'https://saraban63.dpim.go.th:82',
+  apiServer: 'https://saraban63.dpim.go.th',
   apiName: '/pxservice-dpim/api',
   plugIn: 'https://saraban63.dpim.go.th:443',
   url: 'https://saraban63.dpim.go.th/praxticol8_dpim/#',
