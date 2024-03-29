59292d83b94e2475086a33c18163c436bb5f6ac3
Sakul Pavapootanont <idiot_genesis@hotmail.com>
2024-03-29 18:41:31 +0700

    Implement file-attach-template, dialog

--- src/app/setting/file-attach-template/dialog-add-file-attach-template/dialog-add-file-attach-template.component.ts
+++ src/app/setting/file-attach-template/dialog-add-file-attach-template/dialog-add-file-attach-template.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { MdDialog, MdDialogRef } from '@angular/material'
-import { Message } from 'primeng/primeng';
 import { FileUploader } from 'ng2-file-upload';
 import { ParamSarabanService } from '../../../saraban/service/param-saraban.service'
 import { FileAttach } from '../../../main/model/file-attach.model';
@@ -18,13 +17,12 @@ export class DialogAddFileAttachTemplateComponent implements OnInit {
   uploader: FileUploader = new FileUploader({})
   fileName: string = ''
   fileType: string = '.unknown'
-  fileUpdated: boolean = false
   readonly allowedMimeType = this._paramSarabanService.allowedMimeType
   readonly fileSizeLimit = this._paramSarabanService.fileSizeLimit
   readonly fileSizeLimitByte = this._paramSarabanService.fileSizeLimit * 1024 * 1024
 
-  validateSave = () => this.uploader.queue.length != 0 && this.fileName.length != 0
-  validateEdit = () => this.fileName.length != 0
+  validateSave = () => this.uploader.queue.length != 0 && this.fileName
+  validateEdit = () => this.fileName
 
   constructor(
     private _dialog: MdDialog,
@@ -57,14 +55,13 @@ export class DialogAddFileAttachTemplateComponent implements OnInit {
     const last: number = this.uploader.queue.length - 1
     const file: any = this.uploader.queue[last].file
     const typePos: string = file.name.lastIndexOf('.')
-    this.fileName = file.name.substring(0, typePos)
+    if (this.fileAttach.id == 0) this.fileName = file.name.substring(0, typePos)
     this.fileType = file.name.substring(typePos)
 
     const isFileTypeValid = this.isFileTypeValid(file.type)
     const isFileSizeValid = this.isFileSizeValid(file.size)
     if (isFileTypeValid && isFileSizeValid) {
       this.uploader.queue = [this.uploader.queue[last]]
-      this.fileUpdated = true
       return
     } else this.clear()
 
@@ -90,11 +87,17 @@ export class DialogAddFileAttachTemplateComponent implements OnInit {
   clear() {
     this.uploader.queue = []
     this.fileType = '.unknown'
-    this.fileUpdated = false
   }
 
   save() {
-
+    let uploader: FileUploader = null
+    if (this.uploader.queue.length != 0) {
+      uploader = this.uploader
+      uploader.queue[0].file.name = `${this.fileName}${this.fileType}`
+    }
+    this.fileAttach.fileAttachName = `${this.fileName}${this.fileType}`
+    this.fileAttach.fileAttachType = this.fileType.toLowerCase()
+    this._dialogRef.close({ uploader: uploader, fileAttach: this.fileAttach })
   }
 
 }
--- src/app/setting/file-attach-template/file-attach-template.component.ts
+++ src/app/setting/file-attach-template/file-attach-template.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common'
 import { TdLoadingService } from '@covalent/core';
 import { PxService } from '../../main/px.service'
 import { MdDialog } from '@angular/material';
 import { FileAttach } from '../../main/model/file-attach.model';
-// import { DialogAddFileAttachTemplateComponent } from './dialog-add-file-attach-template/dialog-add-file-attach-template.component';
+import { DialogAddFileAttachTemplateComponent } from './dialog-add-file-attach-template/dialog-add-file-attach-template.component';
+import { Message } from 'primeng/primeng';
 
 @Component({
   selector: 'app-file-attach-template',
@@ -14,47 +16,115 @@ import { FileAttach } from '../../main/model/file-attach.model';
 export class FileAttachTemplateComponent implements OnInit {
   fileAttachs: FileAttach[] = []
   hoverEdit: number = -1
+  msgs: Message[] = []
 
   constructor(
+    private _location: Location,
     private _loadingService: TdLoadingService,
     private _dialog: MdDialog,
     private _pxService: PxService
   ) { }
 
   ngOnInit() {
-    this.getFileAttachTEmplates()
+    this.getFileAttachTemplates()
   }
 
-  private getFileAttachTEmplates() {
+  goBack() {
+    this._location.back()
+  }
+
+  private getFileAttachTemplates(msg?: Message) {
     this._loadingService.register('main')
     this._pxService
       .getFileAttachs('wf', 0, 'asc')
-      // .getFileAttachs('dms', 41, 'asc')
       .subscribe(response => {
         this._loadingService.resolve('main')
         this.fileAttachs = response
+
+        if (msg) this.msgs.push(msg)
       })
   }
 
+  mapFileName(fileAttach: FileAttach): string {
+    const name: string = fileAttach.fileAttachName
+    const typePos: number = name.lastIndexOf('.')
+    return name.substring(0, typePos)
+  }
+
   add() {
-    // const dialog = this._dialog.open(DialogAddFileAttachTemplateComponent, {
-    //   width: '80%', height: '90%'
-    // })
+    const dialog = this._dialog.open(DialogAddFileAttachTemplateComponent, {
+      width: '80%', height: '90%'
+    })
+    dialog.afterClosed().subscribe((result: { uploader: any, fileAttach: FileAttach }) => {
+      if (result) {
+        this._loadingService.register('main')
+        this._pxService.uploadList(result.uploader, this.genFileAttachDetail())
+          .onCompleteAll = () => {
+            this._loadingService.resolve('main')
+            this.getFileAttachTemplates(this.genMsg('เพิ่ม', result.fileAttach))
+          }
+      }
+    })
+  }
+
+  private genFileAttachDetail(): any {
+    return {
+      version: 1,
+      linkType: 'wf',
+      linkId: 0,
+    }
+  }
+
+  private genMsg(action: string, fileAttach: FileAttach): Message {
+    return {
+      severity: 'success',
+      summary: `${action}แบบฟอร์ม`,
+      detail: `คุณได้ทำการ${action}แบบฟอร์ม ${this.mapFileName(fileAttach)}`
+    }
   }
 
   edit(fileAttach: FileAttach) {
-    // const dialog = this._dialog.open(DialogAddFileAttachTemplateComponent, {
-    //   width: '80%', height: '90%'
-    // })
-    // dialog.componentInstance.fileAttach = Object.assign({}, fileAttach)
+    const dialog = this._dialog.open(DialogAddFileAttachTemplateComponent, {
+      width: '80%', height: '90%'
+    })
+    dialog.componentInstance.fileAttach = Object.assign({}, fileAttach)
+    dialog.afterClosed().subscribe((result: { uploader: any, fileAttach: FileAttach }) => {
+      if (result) {
+        if (result.uploader) {
+          this._loadingService.register('main')
+          this._pxService.replaceFile(result.uploader, result.fileAttach)
+            .onCompleteAll = () => {
+              this._loadingService.resolve('main')
+              this.getFileAttachTemplates(this.genMsg('แก้ไข', fileAttach))
+            }
+        } else {
+          this._loadingService.register('main')
+          this._pxService
+            .updateFileAttachModel2(result.fileAttach)
+            .subscribe(response => {
+              this._loadingService.resolve('main')
+              this.getFileAttachTemplates(this.genMsg('แก้ไข', fileAttach))
+            })
+        }
+      }
+    })
   }
 
-  delete() {
+  delete(fileAttach: FileAttach) {
+    this._loadingService.register('main')
+    this._pxService
+      .removeFileAttach(fileAttach.id)
+      .subscribe(response => {
+        this._loadingService.resolve('main')
+        const index = this.fileAttachs.indexOf(fileAttach)
+        this.fileAttachs.splice(index, 1)
 
+        this.msgs.push(this.genMsg('ลบ', fileAttach))
+      })
   }
 
-  download() {
-
+  download(fileAttach: FileAttach) {
+    this._pxService.downloadFileAttach(fileAttach)
   }
 
 }
